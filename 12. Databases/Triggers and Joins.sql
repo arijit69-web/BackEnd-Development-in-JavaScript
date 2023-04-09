6f41293dc187814bbd5bece76a2d669a10dd0432
Arijit Sarkar <arijitsrkr13@gmail.com>
2023-04-09 21:32:14 +0530

    update

--- 12. Databases/Triggers and Joins.sql
+++ 12. Databases/Triggers and Joins.sql
@@ -8014,3 +8014,173 @@ SELECT * FROM OrderDetails LIMIT 10 OFFSET 12;
 /* 
 OFFSET and LIMIT clauses are together used for pagination.
 */
+
+/*
+A stored procedure is a prepared SQL code/function that you can save, so 
+the code can be reused over and over again.So if you have an SQL
+query that you write over and over again, save it as a stored 
+procedure, and then just call it to execute it.
+*/
+
+use classicmodels;
+DELIMITER #
+CREATE PROCEDURE showLimitedOrders()
+BEGIN
+     SELECT * FROM ORDERS LIMIT 10;
+END #
+DELIMITER ;
+
+/*Calling the stored procedure*/
+
+CALL showLimitedOrders();
+
+/*
+By default, mysql itself recognizes the semicolon as a statement
+delimiter, so you must redefine the delimiter temporarily to cause
+mysql to pass the entire stored program definition to the server.
+To redefine the mysql delimiter, use the delimiter command.
+
+When u write `DELIMITER #`, for the lines that are written below
+`DELIMITER #`. The DELIMITER for the SQL statement becomes hash(#), that 
+means the moment u put a hash(#) it will consider that character hash(#)
+as a DELIMITER for the valid SQL statement. By default the DELIMITER
+is semicolon(;). But now the DELIMITER is changed to a hash(#). 
+
+The below query is a one valid SQL statement:
+`CREATE PROCEDURE showLimitedOrders()
+BEGIN
+     SELECT * FROM ORDERS LIMIT 10; /* If u will not update the DELIMITER to #(hash), the moment u hit the semicolon(;) on this line, it will assume that this was a valid end of SQL statement which is actually not and it will start throwing an error.
+END #`
+
+DELIMITER ; Atlast updating the DELIMITER back to semicolon(;)
+Common delimiters are commas (,), semicolon (;), quotes ( ", ' ), braces ({}), pipes (|), or slashes ( / \ ).
+*/
+
+DELIMITER $
+CREATE PROCEDURE showLimitedOrdersandOrderDetails()
+BEGIN
+	select * from orders limit 10;
+    select * from orderDetails limit 5;
+END $
+DELIMITER ;
+
+CALL showLimitedOrdersandOrderDetails();
+
+
+
+
+DELIMITER $
+CREATE PROCEDURE passingValuesToOrder(
+IN lt int
+)
+BEGIN
+	select * from orders limit lt;
+END $
+DELIMITER ;
+
+CALL passingValuestoOrder(41);
+
+
+
+
+DELIMITER $
+CREATE PROCEDURE passingLimitAndOrderNumberToOrder(
+IN lt int,IN orderNo varchar(255)
+)
+BEGIN
+	select * from orders where orderNumber = orderNo limit lt;
+END $
+DELIMITER ;
+
+CALL passingLimitAndOrderNumberToOrder(2,'10101');
+
+
+
+
+DELIMITER $
+CREATE PROCEDURE passingOrderNumberToOrderAndCounting(
+IN orderNo varchar(255), OUT cnt int
+)
+BEGIN
+	select count(*) into cnt from orders where orderNumber = orderNo;
+END $
+DELIMITER ;
+
+CALL passingOrderNumberToOrderAndCounting('10101',@cnt);
+select @cnt;
+
+/* 
+@cnt is a session variable. A session variable is a user-defined
+variable (not a server option) that starts with @, does not 
+require declaration, can be used in any SQL query or statement,
+not visible to other sessions, and exists until the end of the 
+current session.
+*/
+
+
+/* Print the first 5 natural numbers using a loop in PLSQL */
+
+DELIMITER $
+CREATE PROCEDURE proc()
+BEGIN
+	declare x INT;
+    set x=1;
+    while x<=5 do
+		Select x;
+		set x=x+1;
+	end while;    
+END $
+DELIMITER ;
+
+CALL proc();
+
+
+/* 
+Trigger is a kind of event listner. A trigger is a named 
+database object that is associated with a table, and that
+activates when a particular event occurs for the table. Some
+uses for triggers are to perform checks of values to be inserted 
+into a table or to perform calculations on values involved in an 
+update.
+
+A trigger is defined to activate when a statement inserts, updates,
+or deletes rows in the associated table. These row operations are
+trigger events. For example, rows can be inserted by INSERT or 
+LOAD DATA statements, and an insert trigger activates for each 
+inserted row. A trigger can be set to activate either before or 
+after the trigger event. For example, you can have a trigger 
+activate before each row that is inserted into a table or after 
+each row that is updated.
+*/
+
+
+DELIMITER $
+
+CREATE Trigger Logger
+Before Insert On Products
+for each row
+	set @log = "adding new product";
+$
+
+DELIMITER ;
+
+set @log ="";
+insert  into products values ('S10_19497','1952 Alpine Renault 2900','Classic Cars','1:10','Arijit Sarkar Creations','Turnable front wheels; steering function; detailed interior; detailed engine; opening hood; opening trunk; opening doors; and detailed chassis.',7305,'98.58','214.30');
+select @log;
+
+
+
+DELIMITER $
+
+CREATE Trigger setPrice
+Before Insert On Products
+for each row
+	set NEW.MSRP=1; /* The NEW variable actually refers to the new MYSQL row that will be inserted inside the table and in that row whatever is the MSRP property that will be set to 1000.*/
+$
+
+DELIMITER ;
+insert  into products values ('S10_19490','1952 Alpine Renault 2100','Classic Cars','1:10','Arijit Sarkar Creations','Turnable front wheels; steering function; detailed interior; detailed engine; opening hood; opening trunk; opening doors; and detailed chassis.',7305,'98.58','214.30');
+/*
+While inserting, the MSRP that we are setting is 214.30 but it has been already updated to 1000 because before insert on any product we are setting the MSRP to be equal to 1000.
+*/
+select * from Products where productCode = 'S10_19490';
